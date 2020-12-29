8b186547c17c3c6f0f94c61253cbfa9840960658
刘意念 <3180399501@qq.com>
2020-12-29 17:12:01 +0800

    添加简单列表页面

--- src/main/java/com/it/favorites/controller/record/RecordController.java
+++ src/main/java/com/it/favorites/controller/record/RecordController.java
@@ -6,9 +6,11 @@ import com.it.favorites.service.record.RecordService;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 @Slf4j
 @RequestMapping("/record")
 @Api(tags = "收藏记录")
@@ -17,12 +19,20 @@ public class RecordController {
     private RecordService recordService;
 
     @GetMapping("/list")
+    @ResponseBody
     public HttpResult getList() {
         return new HttpResult(recordService.findAll());
     }
 
     @PostMapping("/save")
+    @ResponseBody
     public HttpResult save(@RequestBody Record record) {
         return new HttpResult(recordService.save(record));
+    }
+
+    @GetMapping("/show")
+    public String sayHello(Model model) {
+        model.addAttribute("favList", recordService.findAll());
+        return "fav-list";
     }
 }
